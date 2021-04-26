868e8841dcf6026c4f05bd9c0b6048f005e2b690
Ali Adil <aliadilg@gmail.com>
2021-04-26 23:26:20 +0500

    update

--- exercise/lib/main.dart
+++ exercise/lib/main.dart
@@ -11,15 +11,33 @@ void main() => runApp(MyApp());
 
 // Every widget is a flutter class which have build method
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer to my questions");
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s favorite color?', 'What\'s your favorite animal'];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        // Text is a named argument for material app and inside text string is
-        // a precisional arugment
-        child: Text('Ali Adil'),
-      ),
+
+      // home: Center(
+      //   // Text is a named argument for material app and inside text string is
+      //   // a precisional arugment
+      //   child: Text('Ali Adil'),
+      // ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text('The questions!'),
+              ElevatedButton(onPressed: null, child: Text('Answer 1')),
+              ElevatedButton(onPressed: null, child: Text('Answer 2')),
+              ElevatedButton(onPressed: null, child: Text('Answer 3')),
+            ],
+          )),
     );
   }
 }
