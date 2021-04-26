e60aebc6ef1edfb39e3c10399877bd07900415f9
Ali Adil <aliadilg@gmail.com>
2021-04-26 22:26:50 +0500

    update

--- exercise/lib/main.dart
+++ exercise/lib/main.dart
@@ -2,14 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // by using parentheses we are creating MyApp() instance we are instantiating
+  // MyApp widget class here
   runApp(MyApp());
 }
 
+// Every widget is a flutter class which have build method
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Center(
+        // Text is a named argument for material app and inside text string is
+        // a precisional arugment
         child: Text('Ali Adil'),
       ),
     );
